d48abbca4488f851dd8b501661a0a57e23e9c97f
Alex <baskoaleksandr@gmail.com>
2020-03-14 21:17:52 +0300

    // +gradle. CRUD

---
+++ src/main/java/alba/CarHelper/CarHelperApplication.java
@@ -0,0 +1,11 @@
+package alba.CarHelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarHelperApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CarHelperApplication.class, args);
+	}
+}
