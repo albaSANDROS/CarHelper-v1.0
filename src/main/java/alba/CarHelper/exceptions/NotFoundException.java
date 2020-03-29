ae71a4ad7da269d8a86e2f2cd0ed611f7f2e02f4
Alex <baskoaleksandr@gmail.com>
2020-03-29 10:28:45 +0300

    ,

--- src/main/java/alba/CarHelper/exceptions/NotFoundException.java
+++ src/main/java/alba/CarHelper/exceptions/NotFoundException.java
@@ -1,13 +1,11 @@
 package alba.CarHelper.exceptions;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
-//@ResponseStatus(value = HttpStatus.NOT_FOUND)
-//public class NotFoundException extends RuntimeException {
-//}
 
 @ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "There is no such messages")
 public class NotFoundException extends RuntimeException {
+
 }
